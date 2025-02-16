b2886e0d391f9d0dbb75827bf6951b13cce584a3
Will Blankenship <blankenship.will@gmail.com>
2025-02-16 13:40:06 -0800

    Added premake to the project

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+﻿project "GLFW"
+    kind "StaticLib"
+    language "C"
+    staticruntime "Off"
+    
+    targetdir ("bin/" .. outdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outdir .. "/%{prj.name}")
+    
+    files
+    {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        
+        "src/platform.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
+
+filter "system:windows"
+    systemversion "latest"
+    
+    files
+    {
+        "src/win32_init.c",
+        "src/win32_joystick.c",
+        "src/win32_monitor.c",
+        "src/win32_time.c",
+        "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c"
+    }    
+
+    defines
+    {
+        "_GLFW_WIN32",
+        "_CRT_SECURE_NO_WARNINGS"
+    }
+    
+    filter "system:linux"
+        pic "On"
+    
+        systemversion "latest"
+        
+        files
+        {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_module.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/posix_module.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
+        }
+    
+        defines
+        {
+            "_GLFW_X11"
+        }
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        
+    filter {"system:windows", "configurations:Debug"}
+        runtime "Debug"
+        symbols "On"
+        sanitize "Address"
+        editandcontinue "Off"
+        debugformat "Default"
+        flags {"NoRunTimeChecks", "NoIncrementalLink"}
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "Speed"
+        
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "Speed"
+        symbols "Off"
+        
